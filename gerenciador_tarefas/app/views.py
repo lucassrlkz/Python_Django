c6f0c3554bdf37e53e8ffe421c7d2f834ba1498b
lucasv0097 <lucas_vieira97@hotmail.com>
2019-09-09 17:05:53 -0300

    database table and form

--- gerenciador_tarefas/app/views.py
+++ gerenciador_tarefas/app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .forms import TarefaForm
 
 # Create your views here.
 def listar_tarefas(request):
     nome_tarefa ="passando raiva com o bd mysql"
     return render(request, 'tarefas/listar_tarefas.html',{"nome_tarefa":nome_tarefa})
+
+def cadastrar_tarefa(request):
+    form_tarefa = TarefaForm()
+    return render(request, 'tarefas/form_tarefa.html',{"form_tarefa": form_tarefa})
