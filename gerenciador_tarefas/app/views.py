d6eabfa533462d37b78431500ecac610c607f52f
lucasv0097 <lucas_vieira97@hotmail.com>
2019-09-09 15:58:00 -0300

    database configuration

--- gerenciador_tarefas/app/views.py
+++ gerenciador_tarefas/app/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def listar_tarefas(request):
-    nome_tarefa ="passando raiva com o bd mysqli"
+    nome_tarefa ="passando raiva com o bd mysql"
     return render(request, 'tarefas/listar_tarefas.html',{"nome_tarefa":nome_tarefa})
