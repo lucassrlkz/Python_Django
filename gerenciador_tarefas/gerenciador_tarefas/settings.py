2653636e19b1a3147ac4db67708b447dba58b889
lucasv0097 <lucas_vieira97@hotmail.com>
2019-09-08 23:18:53 -0300

    config

--- gerenciador_tarefas/gerenciador_tarefas/settings.py
+++ gerenciador_tarefas/gerenciador_tarefas/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'gerenciador_tarefas',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'admin',
         'HOST': 'localhost',
         'PORT': 3366
     }
